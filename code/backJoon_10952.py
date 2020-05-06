26991b051de404ecc188abc7d55a76231ed369c4
yujin <leeyujin95y@naver.com>
2020-05-06 16:21:14 +0900

    10952번 A+B - 5 변경

--- code/backJoon_10952.py
+++ code/backJoon_10952.py
@@ -1,4 +1,6 @@
-a, b = map(int, input().split())
-while a != 0 and b != 0:
-    print(a+b)
+while True:
     a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    else :
+        print(a+b)
